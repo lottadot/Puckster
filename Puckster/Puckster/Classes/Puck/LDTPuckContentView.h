0e658e2615cff420bb2187ea239247746c9b91d8
Shane Zatezalo <shane@lottadot.com>
2015-04-25 16:33:45 -0400

    Add LDTPuckContentView stub.

---
+++ Puckster/Puckster/Classes/Puck/LDTPuckContentView.h
@@ -0,0 +1,21 @@
+//
+//  LDTPuckContentView.h
+//  Puckster
+//
+//  Created by Shane Zatezalo on 4/25/15.
+//  Copyright (c) 2015 Lottadot LLC. All rights reserved.
+//
+
+@import UIKit;
+
+/**
+ The `LDTPuckContentView` is the `UIView` based class used to display content when the user single-taps on the puck.
+ */
+@interface LDTPuckContentView : UIView
+
+/**
+ Sets the content to be shown. If nil, removes any existing content.
+ */
+- (void)setContent:(UIView *)contentView;
+
+@end
---
+++ Puckster/Puckster/Classes/Puck/LDTPuckContentView.m
@@ -0,0 +1,68 @@
+//
+//  LDTPuckContentView.m
+//  Puckster
+//
+//  Created by Shane Zatezalo on 4/25/15.
+//  Copyright (c) 2015 Lottadot LLC. All rights reserved.
+//
+
+#import "LDTPuckContentView.h"
+#import "UIView+LDTPuckAutoLayoutUtil.h"
+
+@interface LDTPuckContentView ()
+
+/// A `UIView` to contain the content View.
+@property (nonatomic, strong) IBOutlet UIView *contentContainerView;
+
+/// The `UIView` to show within our Container view. Setting to Nil removes.
+@property (nonatomic, strong) UIView *contentView;
+
+@end
+
+@implementation LDTPuckContentView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self addContentContainerView];
+    }
+    return self;
+}
+
+#pragma mark - UI Setup
+
+/**
+ Adds the contentContainerView as a subview and constraints its bounds to self.
+ */
+- (void)addContentContainerView
+{
+    // Add Content Container View
+    [self addSubview:self.contentContainerView];
+    [self LDTPinView:self.contentContainerView toContainer:self];
+}
+
+#pragma mark - iVars
+
+- (UIView *)contentContainerView
+{
+    if (nil == _contentContainerView) {
+        _contentContainerView = [[UIView alloc] initWithFrame:CGRectZero];
+        [_contentContainerView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    }
+    return _contentContainerView;
+}
+
+#pragma mark - Drawing
+
+- (void)drawRect:(CGRect)rect
+{
+    // TODO Maybe a custom shape or something
+}
+
+- (void)setContent:(UIView *)contentView
+{
+    // TODO
+}
+
+@end
