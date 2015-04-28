2cb367b62a17054b6361bf70d2960e720cd2cb11
Shane Zatezalo <shane@lottadot.com>
2015-04-27 21:44:14 -0400

    Add Static Framework build target for iOS7+

---
+++ Puckster/PucksterLibrary/PucksterLibrary.h
@@ -0,0 +1,12 @@
+//
+//  PucksterLibrary.h
+//  PucksterLibrary
+//
+//  Created by Shane Zatezalo on 4/27/15.
+//  Copyright (c) 2015 Lottadot LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <Puckster/LDTPuckControl>
+
