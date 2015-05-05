c0517224d2f9ecff11d353811285884d2d21c5c6
Shane Zatezalo <shane@lottadot.com>
2015-05-04 22:52:05 -0400

    Started to implement test001ElementsExist

--- Puckster/PucksterTests/PucksterTests.m
+++ Puckster/PucksterTests/PucksterTests.m
@@ -13,6 +13,7 @@
 #import "LDTPuckViewDataSource.h"
 #import "LDTPuckContentView.h"
 #import "ViewController.h"
+#import <OCMockFramework/OCMockFramework.h>
 
 @interface PucksterTests : XCTestCase
 
@@ -26,22 +27,6 @@
 - (void)setUp
 {
     [super setUp];
-// TODO
-//    _viewController = [ViewController new];
-//    UIWindow *window = _viewController.view.window;
-//    
-//    [_viewController beginAppearanceTransition:YES animated:YES];
-//    [_viewController loadView];
-//    [_viewController endAppearanceTransition];
-//    
-//    _puckControl = [[LDTPuckControl alloc]
-//                    initInWindow:window
-//                    withLocation:LDTPuckViewLocationBottomRight
-//                    withDelegate:_viewController
-//                    dataSource:_viewController
-//                    puckColor:[UIColor greenColor]
-//                    puckBorderColor:[UIColor blackColor]
-//                    animated:YES];
 }
 
 - (void)tearDown
@@ -52,9 +37,39 @@
 
 - (void)test001ElementsExist
 {
-// TODO
-//    XCTAssertNotNil(_viewController);
-//    XCTAssertNotNil(_puckControl);
+    // TODO Figure out whether we can use our own Window or must mock or...
+    _viewController = [ViewController new];
+    XCTAssertNotNil(_viewController);
+    UIWindow *window = [UIWindow new];  //_viewController.view.window;
+    XCTAssertNotNil(window);
+
+    [window setRootViewController:_viewController];
+    
+    // use OCMock to create a mock delegate object conforming to our custom delegate “LDTPuckControlDataSource”
+    id mockPuckDelegate = [OCMockObject mockForProtocol:@protocol(LDTPuckControlDelegate)];
+    id mockPuckDataSource = [OCMockObject mockForProtocol:@protocol(LDTPuckControlDataSource)];
+    
+    [_viewController beginAppearanceTransition:YES animated:YES];
+    [_viewController loadView];
+    [_viewController endAppearanceTransition];
+    
+    // [[authService expect] loginWithEmail:[OCMArg any] andPassword:[OCMArg any]];
+    // FIXME [[mockPuckDelegate expect] puckBorderColor];
+    [[mockPuckDelegate expect] willPresentPuckWithPuckControl:[OCMArg any]];
+    [[mockPuckDelegate expect] didPresentPuckWithPuckControl:[OCMArg any]];
+    
+    _puckControl = [[LDTPuckControl alloc]
+                    initInWindow:window
+                    withLocation:LDTPuckViewLocationBottomRight
+                    withDelegate:mockPuckDelegate
+                    dataSource:mockPuckDataSource
+                    puckColor:[UIColor greenColor]
+                    puckBorderColor:[UIColor blackColor]
+                    animated:YES];
+    
+    XCTAssertNotNil(_viewController);
+    XCTAssertNotNil(_puckControl);
+    XCTAssertTrue([mockPuckDelegate verify]);
 }
 
 @end
