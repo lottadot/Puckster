2566655cc9d4217fe0c8b9c99a3ad834e3189bb3
Shane Zatezalo <shane@lottadot.com>
2015-05-05 22:18:02 -0400

    Add logical unit tests
    * test001PuckExist, test002PuckSingleTaps, test002PuckDoubleTaps

--- Puckster/PucksterTests/PucksterTests.m
+++ Puckster/PucksterTests/PucksterTests.m
@@ -14,6 +14,13 @@
 #import "ViewController.h"
 #import <OCMockFramework/OCMockFramework.h>
 
+@interface LDTPuckControl (TestPrivate)
+
+- (IBAction)puckSingleTapped:(id)sender;
+- (IBAction)puckDoubleTapped:(id)sender;
+
+@end
+
 @interface PucksterTests : XCTestCase
 
 @property (nonatomic, strong) UIWindow *window;
@@ -42,7 +49,8 @@
     [super tearDown];
 }
 
-- (void)test001ElementsExist
+/// Test that the puck appears and the proper delegate messages are sent to indicate it will/did present.
+- (void)test001PuckExist
 {   
     // use OCMock to create a mock delegate object conforming to our custom delegate “LDTPuckControlDataSource”
     id mockPuckDelegate = [OCMockObject mockForProtocol:@protocol(LDTPuckControlDelegate)];
@@ -69,6 +77,27 @@
     XCTAssertNotNil(_viewController);
     XCTAssertNotNil(_puckControl);
     XCTAssertTrue([mockPuckDelegate verify]);
+}
+
+/// Test that the proper delegate messages are sent when the user single-taps the puck
+- (void)test002PuckSingleTaps
+{
+    id mockPuckDelegate = [OCMockObject mockForProtocol:@protocol(LDTPuckControlDelegate)];
+    
+    [_puckControl puckSingleTapped:nil];
+    [[mockPuckDelegate expect] didSelectPuckWithPuckControl:[OCMArg any]];
+    [[mockPuckDelegate expect] willPresentPuckWithPuckControl:[OCMArg any]];
+    [[mockPuckDelegate expect] didPresentPuckWithPuckControl:[OCMArg any]];
+}
+
+/// Test that the proper delegate messages are sent when the user double-taps the puck
+- (void)test002PuckDoubleTaps
+{
+    id mockPuckDelegate = [OCMockObject mockForProtocol:@protocol(LDTPuckControlDelegate)];
+    
+    [_puckControl puckDoubleTapped:nil];
+    [[mockPuckDelegate expect] willDismissPuckWithPuckControl:[OCMArg any]];
+    [[mockPuckDelegate expect] didDismissPuckWithPuckControl:[OCMArg any]];
 }
 
 @end
